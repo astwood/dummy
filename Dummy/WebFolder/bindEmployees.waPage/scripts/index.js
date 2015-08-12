2455d3dda2328f7002ebf346d0d3bf3cd0aa308f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-08-12 09:32:32 +0100

    Update #3
    Update #3

---
+++ Dummy/WebFolder/bindEmployees.waPage/scripts/index.js
@@ -0,0 +1,21 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		// Create the assoication
+		sources.employee.employer.set(sources.company);
+		// Save it
+		sources.employee.save();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
